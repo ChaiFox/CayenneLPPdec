250619dd44c5dacea105a3f2d3ae5b0527a37b47
ChaiFox <ChaiFox@live.com>
2023-12-27 11:52:58 -0700

    Update CayenneLPPdec_test.ino
    Updated array creation for ArduinoJSON 6.
    Minor formatting changes for preference.

--- examples/CayenneLPPdec_test/CayenneLPPdec_test.ino
+++ examples/CayenneLPPdec_test/CayenneLPPdec_test.ino
@@ -4,39 +4,39 @@
 #include <CayenneLPP.h>
 #include "helperFunctions.h"
 
-void setup () {
-    Serial.begin (115200); Serial.println (); Serial.println ();
+void setup() {
+    Serial.begin(115200); Serial.println(); Serial.println();
 
     // First, we create a CayenneLPP buffer to hold test data to decode
-    CayenneLPP lpp (51);
+    CayenneLPP lpp(51);
 
-    lpp.reset ();
+    lpp.reset();
 
     // Fill CayenneLPP buffer with some data
-    lpp.addTemperature (1,25.564);
-    lpp.addUnixTime (2, millis());
-    lpp.addGPS (3, 4.34, 40.22, 755);
+    lpp.addTemperature(1,25.564);
+    lpp.addUnixTime(2, millis());
+    lpp.addGPS(3, 4.34, 40.22, 755);
 
-    uint8_t *buffer = lpp.getBuffer ();
-    uint16_t len = lpp.getSize ();
+    uint8_t *buffer = lpp.getBuffer();
+    uint16_t len = lpp.getSize();
 
     // Dump buffer content for debugging
-    Serial.printf ("LPP Buffer: %s\n\n", printHexBuffer (buffer, len));
+    Serial.printf("LPP Buffer: %s\n\n", printHexBuffer(buffer, len));
 
     // Create a Json buffer big enough. You can use https://arduinojson.org/v5/assistant/ to do a calculation
-	StaticJsonDocument<512> jsonBuffer;
+    StaticJsonDocument<512> jsonBuffer;
 
     // Create an array to parse data to
-	JsonArray root = jsonBuffer.createNestedArray();
+    JsonArray root = jsonBuffer.to<JsonArray>();
 
     // Call parser
-    CayenneLPPDec::ParseLPP (buffer, len, root);
+    CayenneLPPDec::ParseLPP(buffer, len, root);
 
     // Print JSON data to serial
-	serializeJsonPretty (root, Serial);
+    serializeJsonPretty(root, Serial);
 
 }
 
-void loop () {
+void loop() {
 
 }
